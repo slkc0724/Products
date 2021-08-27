5ddd379a20ec402ebd835b7d59f3f2fb05453a10
slkc0724 <88265542+slkc0724@users.noreply.github.com>
2021-08-27 20:49:33 +0800

    Add files via upload

--- products.py
+++ products.py
@@ -4,7 +4,12 @@ while True:
 	if name == 'finish':
 		break
 	price = input('The price of the product: ')
+	price = float(price)
 	products.append([name, price])
 print(products)	
 for p in products:
 	print('The price of', p[0], 'is', p[1])
+
+with open('products.csv', 'w') as f:
+	for p in products:
+		f.write(p[0] + ',' + str(p[1]) + '\n')
